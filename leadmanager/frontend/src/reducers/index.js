a2c38f8ebbba82e7b363e3033823590fc1aa83cf
Nicolas G Esser <nicolas@Nicolass-MacBook-Air.local>
2020-07-14 20:10:14 -0500

    Set up basic auth and react/redux.

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from "redux";
 
-export default combineReducers({});
+import leads from "./leads";
+import errors from "./errors";
+import messages from "./messages";
+import auth from "./auth";
+
+export default combineReducers({ leads, errors, messages, auth });
